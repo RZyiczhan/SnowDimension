f9134096a68c75cba21ec9b229c68d34af2f2822
RZyiczhan <2088915035@qq.com>
2025-10-15 13:12:30 +0800

    start the project with template

---
+++ snowdimension-1.21.1/src/client/java/com/rzero/snowdimension/SnowDimensionClient.java
@@ -0,0 +1,10 @@
+package com.rzero.snowdimension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SnowDimensionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
